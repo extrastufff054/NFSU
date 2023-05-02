743bedba5577ee20b9b44f2419b84dff8277b368
Sarang Vehale <108084022+extrastufff054@users.noreply.github.com>
2023-05-02 16:06:24 +0530

    Cryptography experiment file

---
+++ Cryptography/LabReport/CryptographyEx.cpp
@@ -0,0 +1,9 @@
+Implement eucladian algorithm to find the gcd between two numbers
+Implement extended eucladian to find the inverse of any integer
+Implement caesar cipher,additive and multiplictive cipher
+Implement hill cipher and vigenere cipher
+Implement DES for encryption
+Implement DES for decryption
+Implement AES
+Implement RSA
+Implement Diffie Hellmann
