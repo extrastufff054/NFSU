300cfbf7544af8e70f1d4e0caf72ad5efb1e7a1d
Sarang Vehale <108084022+extrastufff054@users.noreply.github.com>
2023-05-05 17:28:23 +0530

    Update mycode.asm

--- Practicals(Lab)/23-03-2023/mycode.asm
+++ Practicals(Lab)/23-03-2023/mycode.asm
@@ -2,7 +2,7 @@
 
 org 100h
 
-MOV AX, 0FFF0H; add your code here //VALUE OF AX
+MOV AX, 0100H; add your code here //VALUE OF AX
 MOV CX, 0001H;        //VALUE OF CX
 MOV [1234H], AX;    //ADDRESS WHERE VALUE IS STORED
 MOV [1236H], CX;
