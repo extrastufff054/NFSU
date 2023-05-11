0f6aa1a624bbc3d4df7f8d41a205a4b870ce20b2
Sarang Vehale <108084022+extrastufff054@users.noreply.github.com>
2023-05-11 16:23:55 +0530

    Create noOf1.asm

---
+++ Practicals(Lab)/11-05-2023/noOf1.asm
@@ -0,0 +1,14 @@
+MOV CX, 8 ;initialize loop counter to 8
+MOV BL, byte_to_count ;load byte to count into BL
+MOV AH, 0 ;initialize count to 0
+LOOP_START:
+    SHR BL, 1 ;shift right to check each bit
+    JC ADD_COUNT ;if carry flag is set, add to count
+    LOOP LOOP_START ;continue looping until all bits checked
+ADD_COUNT:
+    INC AH ;increment count
+    LOOP LOOP_START ;continue looping until all bits have been checked
+
+byte_to_count EQU 0A5h ;define byte to count as a constant
+
+RET ;return from the program
