db750de59c218b32e9c685f0f75a88ca547b90c0
Sarang Vehale <108084022+extrastufff054@users.noreply.github.com>
2023-06-15 16:43:20 +0530

    Create 15-06-2023.asm

---
+++ Assembly-Language 8051/15-06-2023.asm
@@ -0,0 +1,4 @@
+;Q1 : Write the program to count all 1's
+;Q2 : To perform ADD of 2x2 matrix
+;Q3 : To perform the transpose of 2x2 matrix
+;Q4 : Write an assembly language program to find whether a given byte is available in the given sequence or not. If it is available write FF in R3. Otherwise write 00 in R3.
