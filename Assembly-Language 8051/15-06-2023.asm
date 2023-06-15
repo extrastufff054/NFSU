6aa45455903ee1c7c231902f26832f0cf80c78af
Sarang Vehale <108084022+extrastufff054@users.noreply.github.com>
2023-06-15 17:15:23 +0530

    Update 15-06-2023.asm

--- Assembly-Language 8051/15-06-2023.asm
+++ Assembly-Language 8051/15-06-2023.asm
@@ -2,3 +2,19 @@
 ;Q2 : To perform ADD of 2x2 matrix
 ;Q3 : To perform the transpose of 2x2 matrix
 ;Q4 : Write an assembly language program to find whether a given byte is available in the given sequence or not. If it is available write FF in R3. Otherwise write 00 in R3.
+
+org 00H
+MOV R0, #20H
+MOV R2, #30H
+MOV R1, #40H
+MOV R3,	#04H
+LOOP1: MOV A, @R0
+		MOV B,@R2
+		ADD A, B
+		MOV @R1, A
+		DEC R3
+		INC R0
+		INC R1
+		INC R2
+		CJNE R3, #00H, LOOP1
+END
